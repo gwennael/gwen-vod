5eb79992a89b8e5dbff7a9025899142141da8efd
gwennael1 <gwennael.martin.pro@gmail.com>
2018-01-31 02:52:38 +0100

    changement de la video par defaut du lecteur

--- src/components/PlayerPage.js
+++ src/components/PlayerPage.js
@@ -36,7 +36,7 @@ class PlayerPage extends Component {
                     playsInline
                     ref="player"
                     poster={(this.currentFilm && this.currentFilm.poster_path) ? IMG_POSTER_TMDb_URL+this.currentFilm.poster_path : (this.currentFilm) ? "../img/Image-not-available_1.jpg" : IMG_POSTER_TMDb_URL+"/cGOPbv9wA5gEejkUN892JrveARt.jpg"}
-                    src="http://videos.hd-trailers.net/BatmanvSuperman_TLR-1_5.1-480p-HDTN.mp4"
+                    src="//fr.vid.web.acsta.net/nmedia/33/15/08/04/10/19555513_hd_013.mp4"
                 />
                 <button onClick={this.changeCurrentTime(-30)} type="button" className="btn btn-default col-12 col-sm-6 col-xs-12">-30 sec</button>
                 <button onClick={this.changeCurrentTime(+30)} type="button" className="btn btn-default col-12 col-sm-6 col-xs-12">+30 sec</button>
