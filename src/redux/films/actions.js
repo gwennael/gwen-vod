3174e42d3f58334538cb9d8a9e5fa640285b7571
gwennael1 <gwennael.martin.pro@gmail.com>
2018-02-02 01:03:01 +0100

    fix films filter

--- src/redux/films/actions.js
+++ src/redux/films/actions.js
@@ -9,9 +9,9 @@ let date = new Date();
 
 // Définition des URLs pour les différents filtres
 let filter_URL = {
-    0 : `${API_TMDb_DISCOVER_URL}&primary_release_date.lte=${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}&sort_by=release_date.desc&region=fr`,
+    0 : `${API_TMDb_DISCOVER_URL}&release_date.lte=${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}&sort_by=release_date.desc&region=fr`,
     1: `${API_TMDb_DISCOVER_URL}&sort_by=popularity.desc&region=fr`,
-    2: `${API_TMDb_DISCOVER_URL}&primary_release_date.gte=${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}&sort_by=release_date.asc&region=fr`,
+    2: `${API_TMDb_DISCOVER_URL}&primary_release_date.gte=${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}&sort_by=primary_release_date.asc&region=fr`,
     3: `${API_TMDb_DISCOVER_URL}&primary_release_year=1995&sort_by=popularity.desc&region=fr`
 };
 
